2e0bc42687bb2904e6ad38ee227a249c2a53b557
jcardus <asklocation.net@gmail.com>
2024-08-26 17:37:58 +0100

    log url

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -2,6 +2,7 @@ export function onRequest({request, env}) {
     const url = new URL(request.url.replace('https://', 'http://'))
     url.host = env.TRACCAR_SERVER
     try {
+        console.log(url)
         return fetch(new Request(url, request))
     } catch (e) {
         console.error(e)
