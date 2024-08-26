03dc6a1d22348a47b348d66f090886139a3a571c
jcardus <asklocation.net@gmail.com>
2024-08-26 21:14:56 +0100

    log response code

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,10 +1,10 @@
 export async function onRequest({request, env}) {
     const url = new URL(request.url.replace('https://', 'http://'))
     url.host = env.TRACCAR_SERVER
-        console.log(url)
-        const response = await fetch(new Request(url, request))
+    console.log(url)
+    const response = await fetch(new Request(url, request))
     if (!response.ok) {
-        console.log(await response.text())
+        console.log(response.status, await response.text())
         return new Response('server error ' + response.status, { status: response.status });
     } else {
         return response
